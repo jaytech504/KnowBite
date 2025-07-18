a729e7c17309a92fa6629d2993f0b699013632f1
jaytech524 <jasonyakubu543@gmail.com>
2025-07-18 19:15:52 +0100

    done

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'cloudinary_storage',
-    'cloudinary'
+    'cloudinary',
     # Allauth for social login
     'django.contrib.sites',
     'allauth',
