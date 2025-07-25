d272c4a6b4deffc1c532ea6f20e0dfb4547d6ad7
jaytech524 <jasonyakubu543@gmail.com>
2025-07-25 21:17:31 +0100

    fixed google auth

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -81,11 +81,6 @@ AUTHENTICATION_BACKENDS = [
 # Google OAuth2 settings
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        'APP': {
-            'client_id': os.getenv('GOOGLE_CLIENT_ID'),
-            'secret': os.getenv('GOOGLE_CLIENT_SECRET'),
-            'key': ''
-        },
         'SCOPE': [
             'profile',
             'email',
